7e7246b29dba4d22f03a3a709720d05af7df5e21
Adam <downeyadam0312@gmail.com>
2022-08-19 13:40:37 +0100

    First Commit
    Worked on this project outside of Github. Bringing whole project into a repo

---
+++ CA1_Refreshments/app/src/main/java/com/example/ca1_refreshments/SpoonacularData.kt
@@ -0,0 +1,10 @@
+package com.example.ca1_refreshments
+
+/*
+List of JSON items. This is for correct formatting of JSON file, as there is an object instead of
+an array at the start of the JSON file
+ */
+
+data class SpoonacularData(
+    val results: List<Result>
+)
