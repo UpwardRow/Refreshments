a6d429cc9b595a56057678933cd054c4ea8607ea
UpwardRow <downeyadam0312@gmail.com>
2023-07-04 18:52:15 +0100

    Made some security changes to allow the passwords to be safe from other people in the application

--- app/src/main/java/com/adowney/refreshments/APIInterface.kt
+++ app/src/main/java/com/adowney/refreshments/APIInterface.kt
@@ -4,16 +4,13 @@ import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Query
 
-const val str = "cheese"
-const val burger = "burger"
-
 interface APIInterface {
 
     val ingredients : String
     val searchBarValue : String
 
     //Getting input from FiltersActivity and putting into the link String
-    @GET("recipes/complexSearch?apiKey=***REMOVED***")
+    @GET("recipes/complexSearch?apiKey=")
     fun getSpoonacularData(
         @Query("excludeIngredients") ingredients : List<String>,
         @Query("query") searchBarValue : String
