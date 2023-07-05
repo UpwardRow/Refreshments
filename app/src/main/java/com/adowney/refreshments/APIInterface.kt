48389ba541671b19dfc385a257c9fed3e99e7797
UpwardRow <downeyadam0312@gmail.com>
2023-07-05 17:46:52 +0100

    Added more security to the application, hopefully this won't break the branches

--- app/src/main/java/com/adowney/refreshments/APIInterface.kt
+++ app/src/main/java/com/adowney/refreshments/APIInterface.kt
@@ -4,14 +4,14 @@ import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Query
 
+// This is where the API key is inserted into the url, which is hidden from potential threats
+const val API_KEY = BuildConfig.SPOONACULAR_API_KEY
 interface APIInterface {
-
-    val ingredients : String
-    val searchBarValue : String
 
     //Getting input from FiltersActivity and putting into the link String
     @GET("recipes/complexSearch?apiKey=")
     fun getSpoonacularData(
+        @Query(API_KEY) apiKey : String,
         @Query("excludeIngredients") ingredients : List<String>,
         @Query("query") searchBarValue : String
     ): Call<SpoonacularData>
